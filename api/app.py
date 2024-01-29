e6316ec98219b5dfdb9c42e4cf2b3fd4b349b4a8
mariashapo <mart99maria@gmail.com>
2024-01-29 19:56:51 +0000

    Added environmental varaible and implemented a form for filtering

--- api/app.py
+++ api/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, render_template
 import requests
 
@@ -6,35 +7,57 @@ app = Flask(__name__)
 @app.route('/books', methods=['GET'])
 def consume_service():
     # The URL of the first service
-    service_url = "http://book-api-server-vadim-mariia.h4h0dqcndhhch8ga.uksouth.azurecontainer.io:5000/books"
+    default_url = "http://book-api-server-vadim-mariia.h4h0dqcndhhch8ga.uksouth.azurecontainer.io:5000/books"
+    service_url = os.getenv('BOOKS_API_URL', default_url)
     
-    # Get the genre query parameter
+    # Get query parameters
     genre = request.args.get('genre')
     author = request.args.get('author')
     title = request.args.get('title')
 
-    params = {'genre': genre, 'author': author, 'title': title}
+    # Create a dictionary of parameters to send in the request
+    params = {}
+    if genre:
+        params['genre'] = genre
+    if author:
+        params['author'] = author
+    if title:
+        params['title'] = title
     
-    response = requests.get(service_url)
-    books = response.json()
+    # Send request with parameters    
+    response = requests.get(service_url, params=params)
+    books_response = response.json()
+
+    return render_template('index.html', books=books_response, params=params)
+
+@app.route('/books', methods=['GET', 'POST'])
+def index():
+    # Call the books API with parameters
+    service_url = os.getenv('BOOKS_API_URL')
     
-    filtered_books = []
+    if request.method == 'POST':
+        # Extract data from form
+        genre = request.form.get('genre')
+        author = request.form.get('author')
+        title = request.form.get('title')
+        
+        params = {'genre': genre, 'author': author, 'title': title}
+        response = requests.get(service_url, params=params)
+        books_response = response.json()
 
-    # Check if genre or author filter is applied
-    if genre or author or title:
-        for book in books:
-            genre_match = str.lower(genre) in str.lower(book['genre']) if genre else True
-            author_match = str.lower(author) in str.lower(book['author']) if author else True
-            title_match = str.lower(title) in str.lower(book['title']) if title else True
+        return render_template('index.html', books=books_response, params=params)
 
-            # Add the book if it matches both genre and author criteria
-            if genre_match and author_match and title_match:
-                filtered_books.append(book)
     else:
-        # If no filters are provided, use all books
-        filtered_books = books
-
-    return render_template('index.html', books=filtered_books, params=params)
+        # Get query parameters
+        genre = request.args.get('genre')
+        author = request.args.get('author')
+        title = request.args.get('title')
+        
+        params = {'genre': genre, 'author': author, 'title': title}
+        response = requests.get(service_url, params=params)
+        books_response = response.json()
+        
+        return render_template('index.html', books=None, params=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
